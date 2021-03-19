ff4eca8a56fc48cc758c6f76184b798c4e63c539
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 10:23:40 -0700

    Add controller for products#udpate with flash message

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,6 +29,16 @@ class ProductsController < ApplicationController
     render :edit
   end
 
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated!"
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
   private
     def product_params
       params.require(:product).permit(:name, :genre)
