18b7ecceb4b5c3544a3e9629eb378836eb3208e3
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 13:10:08 -0700

    Validate destroy dependency of product review

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'associations' do
-    it { should have_many(:reviews) }
+    it { should have_many(:reviews).dependent(:destroy) }
   end
 
   describe 'validations' do
