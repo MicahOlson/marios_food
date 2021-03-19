3d293382eb026ae270b9518f6951a507c401d0e8
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 13:21:03 -0700

    Validate presence of review content

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
   validates :author, length: { maximum: 30 }
+  validates :content_body, presence: true
 end
