a35b54f792e24241c8570ba755147a9d6cb6c02e
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 10:50:51 -0700

    Add controller and view for reviews#new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,10 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
     render :index
   end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
 end
