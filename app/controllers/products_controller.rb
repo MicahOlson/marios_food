47f321b4473f49fb5c180bb0ada38ffa4da5d16b
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 10:26:06 -0700

    Add controller for products#destroy with flash message

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,6 +39,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product successfully deleted!"
+    redirect_to products_path
+  end
+
   private
     def product_params
       params.require(:product).permit(:name, :genre)
