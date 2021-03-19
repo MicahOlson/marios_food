e266516e505c8bac0a82432fcd8beb138e95a317
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 12:41:23 -0700

    Validate uniqueness of product name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Product, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
