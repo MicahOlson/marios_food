091cd47475e157cf59c61c89c0f9c6d64329e8c4
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 14:35:58 -0700

    Add titleize callback and remove uniqueness check

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,17 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
-  validates :name, uniqueness: true
   validates :name, length: { maximum: 60 }
   validates :cost, presence: true
   validates :cost, numericality: { greater_than_or_equal_to: 0 }
   validates :country_of_origin, presence: true
   validates :country_of_origin, length: { maximum: 60 }
+
+  before_save(:titleize)
+
+  private
+    def titleize
+      self.name = self.name.titleize
+      self.country_of_origin = self.country_of_origin.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,11 +7,17 @@ RSpec.describe Product, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
     it { should validate_length_of(:name).is_at_most(60) }
     it { should validate_presence_of(:cost) }
     it { should validate_numericality_of(:cost).is_greater_than_or_equal_to(0) }
     it { should validate_presence_of(:country_of_origin) }
     it { should validate_length_of(:country_of_origin).is_at_most(60) }
+  end
+
+  describe 'Product#titleize' do
+    it 'titleizes the product name and country of origin ' do
+      product = Product.create({name: 'corn on the cob', cost: 1.99, country_of_origin: 'argentina'})
+      expect([product.name, product.country_of_origin]).to eq(["Corn On The Cob", "Argentina"])
+    end
   end
 end
