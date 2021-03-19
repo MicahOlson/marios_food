84d89dc280603e0db81a435df2b07463907d613d
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 10:42:24 -0700

    Fix code spacing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,6 +48,6 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.require(:product).permit(:name, :genre)
+      params.require(:product).permit(:name, :cost, :country_of_origin)
     end
 end
