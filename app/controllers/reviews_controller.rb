d787335931942d559304bfe7e0256e76064a5284
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 10:54:31 -0700

    Add controller for reviews#create with flash message

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,4 +9,20 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.new
     render :new
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
 end
