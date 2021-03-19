d246dec65d1cdba2d050be89dd7fb65b31d6beda
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 12:44:54 -0700

    Validate length of product name

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :name, length: { maximum: 60 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Product, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
+    it { should validate_length_of(:name).is_at_most(60) }
   end
 end
