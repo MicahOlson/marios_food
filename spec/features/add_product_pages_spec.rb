802137d0e22a8940bf4dd0ae75b73624c46346e6
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-19 19:46:51 -0700

    Revert to convention over configuration for cost

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe "the add a product process" do
   it "adds a new product" do
     visit products_path
-    click_link 'Create new product'
-    fill_in 'Name', :with => 'corn on the cob'
-    fill_in 'Cost', :with => '1.99'
-    fill_in 'Country of origin', :with => 'argentina'
+    click_link 'Add a product'
+    fill_in 'Name', with: 'corn on the cob'
+    fill_in 'Cost', with: '1.99'
+    fill_in 'Country of origin', with: 'argentina'
     click_on 'Create Product'
     expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Corn On The Cob'
