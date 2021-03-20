bfd37d0572e7f0d4dc8ca77d9ef5e04ce5fc658a
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-20 15:09:16 -0700

    Clean up routes to 'except' reviews index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,4 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-    render :index
-  end
-
   def new
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   resources :products do
-    resources :reviews
+    resources :reviews, except: [:index]
   end
 end
