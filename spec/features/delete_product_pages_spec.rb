ccd21a91bbea77441d4099404b30db3b145689a7
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-20 14:16:29 -0700

    Refactor to new product name to avoid test conflicts

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "the delete a product process", type: :feature do
   it "deletes a product" do
-    test_product = Product.create(name: 'corn on the cob', cost: '1.99', country_of_origin: 'argentina')
+    test_product = Product.create(name: 'goat cheese', cost: '3.99', country_of_origin: 'greece')
     visit product_path(test_product)
     click_link 'delete'
     expect(page).to have_content 'Product successfully deleted!'
-    expect(page).not_to have_content 'Corn On The Cob'
+    expect(page).not_to have_content 'Goat Cheese'
   end
 end
