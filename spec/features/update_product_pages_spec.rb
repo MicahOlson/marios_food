7a807f49a8086962709e676311950fae36e0b0b8
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-20 12:55:40 -0700

    Create integration test to update a product

---
+++ spec/features/update_product_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "the update a product process" do
+  it "updates a product" do
+    test_product = Product.create(name: 'corn on the cob', cost: '1.99', country_of_origin: 'argentina')
+    visit product_path(test_product)
+    click_link 'edit'
+    fill_in 'Name', with: 'green pepper'
+    fill_in 'Cost', with: '.79'
+    fill_in 'Country of origin', with: 'canada'
+    click_on 'Update Product'
+    expect(page).to have_content 'Product successfully updated!'
+    expect(page).to have_content 'Green Pepper'
+  end
+end
