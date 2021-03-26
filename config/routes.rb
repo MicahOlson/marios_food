068010c56f3d54939ee5d0a2486ae26b81a9d923
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-26 08:55:34 -0700

    Add user and session controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
     resources :reviews, except: [:index]
   end
   get "search", :to => "products#index"
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
