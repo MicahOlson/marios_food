c51a20f0a4d4a0959f54ff33c683be8984bec03f
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-26 16:52:19 -0700

    Add simplecov and troubleshoot auth issues with tests

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ end
 group :test do
   gem 'shoulda-matchers'
   gem 'capybara'
+  gem 'simplecov', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -2,19 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "the add a product process", type: :feature do
   it "adds a new product" do
+    visit signin_path
+    fill_in 'email', with: 'admin'
+    fill_in 'Password', with: 'Administrator1!'
+    click_button 'Sign in'
     visit products_path
     click_link 'Add a product'
     fill_in 'Name', with: 'corn on the cob'
     fill_in 'Cost', with: '1.99'
     fill_in 'Country of origin', with: 'argentina'
-    click_on 'Create Product'
+    click_button 'Create Product'
     expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Corn On The Cob'
   end
 
   it "gives an error when no name is entered" do
+    visit signin_path
+    fill_in 'email', with: 'admin'
+    fill_in 'Password', with: 'Administrator1!'
+    click_button 'Sign in'
     visit new_product_path
-    click_on 'Create Product'
+    click_button 'Create Product'
     expect(page).to have_content "Name can't be blank"
   end
 end
