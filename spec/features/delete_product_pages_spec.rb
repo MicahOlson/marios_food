a2c5ddaac2bed5e2d6d970ad392dc461a590a16c
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-29 19:08:02 -0700

    Hide unauthorized links from standard users

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -9,12 +9,4 @@ RSpec.describe "the delete a product process", type: :feature do
     expect(page).to have_content 'Product successfully deleted!'
     expect(page).not_to have_content 'Goat Cheese'
   end
-
-  it "gives an error for unauthorized users" do
-    make_test_user
-    test_product = Product.create(name: 'gula melaka', cost: '7.99', country_of_origin: 'india')
-    visit product_path(test_product)
-    click_link 'delete'
-    expect(page).to have_content "You must be signed in as administrator to perform that action."
-  end
 end
